954aa53f0887c15f72493142d552cf16b1ebac94
K1ng-T0ast <kylejwhite@proton.me>
2023-05-04 13:20:05 -0700

    splash page styling, added scss module

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,14 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
-const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+const LoginButton = (props) => {
+  const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button onClick={() => loginWithRedirect()} className={props.className}>
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
